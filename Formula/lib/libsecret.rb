ee7ca0b9b786ee7b96b59f4bcfc2c571a3378480
Abhijeet Sharma <meetabhijeet05@gmail.com>
2026-09-13 00:58:32 +0530

    libsecret 0.21.8

--- Formula/lib/libsecret.rb
+++ Formula/lib/libsecret.rb
@@ -1,8 +1,8 @@
 class Libsecret < Formula
   desc "Library for storing/retrieving passwords and other secrets"
   homepage "https://wiki.gnome.org/Projects/Libsecret"
-  url "https://download.gnome.org/sources/libsecret/0.21/libsecret-0.21.7.tar.xz"
-  sha256 "6b452e4750590a2b5617adc40026f28d2f4903de15f1250e1d1c40bfd68ed55e"
+  url "https://download.gnome.org/sources/libsecret/0.21/libsecret-0.21.8.tar.xz"
+  sha256 "70e7279ccc7e8cce1bb0e1c57a3785e2c79aea6d1dd9a538abda3886f11cdf72"
   # Excluding GPL-2.0-or-later and Apache-2.0 which are for tests
   license "LGPL-2.1-or-later"
   compatibility_version 1
